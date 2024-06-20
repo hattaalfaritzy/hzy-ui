9f63adb4087ed4e48a2929bce4ea5cb7b0bf7344
Hatta Alfaritzy <muh.hara16@gmail.com>
2024-06-21 04:29:48 +0700

    fix: fix rollup import css

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/react";
 import { themes } from "@storybook/theming";
-import '@/styles/index.css';
+import "../dist/index.css";
 
 const preview: Preview = {
   parameters: {
