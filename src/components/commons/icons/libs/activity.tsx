ce4a9e3e247355102d3c31bc5c41ce8b12d90b85
Hatta Alfaritzy <muh.hara16@gmail.com>
2024-07-02 03:26:37 +0700

    fix: icons attribute

--- src/components/commons/icons/libs/activity.tsx
+++ src/components/commons/icons/libs/activity.tsx
@@ -14,9 +14,9 @@ export default function Activity({ width = 20, ...props }: ISvgProps) {
     >
       <path
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M3 12h4.5L9 6l4 12l2-9l1.5 3H21"
       />
     </svg>
