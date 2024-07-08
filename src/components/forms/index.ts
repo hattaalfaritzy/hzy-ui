da4839b65e73cead7cfc35819dad16307a0ae016
Hatta Alfaritzy <muh.hara16@gmail.com>
2024-07-09 02:50:34 +0700

    fix: import forms

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -3,5 +3,6 @@ export * from "./input-checkbox";
 export * from "./input-password";
 export * from "./input-search";
 export * from "./input-select";
+export * from "./input-switch";
 export * from "./input-text";
 export * from "./input-textarea";
