7e9d1e2a30cd5b54b62faa6b900ff7a98a102c53
Hatta Alfaritzy <muh.hara16@gmail.com>
2025-01-07 03:56:18 +0700

    (master): feat: docs for icons

--- src/components/commons/icons/icons.stories.tsx
+++ src/components/commons/icons/icons.stories.tsx
@@ -1,46 +1,41 @@
-import React from "react";
 import type { Meta, StoryFn } from "@storybook/react";
 import Icons, { icons } from "./";
 
-const meta: Meta<typeof Icons> = {
+export default {
   title: "Components/Commons/Icons",
   component: Icons as Meta<typeof Icons>["component"],
-  tags: ["autodocs"],
-  args: {
-    name: "info",
-  },
+} as Meta<typeof Icons>;
+
+export const IconDefault: StoryFn<typeof Icons> = (args) => {
+  return <Icons {...args} name="activity" />;
 };
 
-export default meta;
-
-const Template: StoryFn<typeof Icons> = (args) => {
-  return <Icons {...args} name={args.name} />;
-};
-
-export const Base = Template.bind({});
-
-const List = () => {
-  const iconsNames = Object.keys(icons);
+export const IconCustomized: StoryFn<typeof Icons> = (args) => {
   return (
-    <div className="flex flex-col space-y-6 mix-blend-difference p-20">
-      <span className="text-lg lg:text-2xl text-white/70">
-        List Components Icons
-      </span>
-      <div className="grid grid-cols-2 lg:grid-cols-6 gap-4 h-fit-content">
-        {Object.values(icons).map((Icon: any, index) => (
-          <div
-            key={index}
-            className="flex flex-col justify-center items-center py-6 border-2 border-white/70 rounded-lg space-y-3"
-          >
-            <Icon className="fill-white/70 stroke-white/70" width={20} />
-            <div className="text-xxs truncate text-white/70">
-              {iconsNames[index]}
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="custom-canvas">
+      <Icons {...args} name="broken-image" className="w-8 h-8 fill-blue-500" />
+      <Icons {...args} name="activity" className="w-8 h-8 stroke-blue-500" />
     </div>
   );
 };
 
-export const ListIcons: StoryFn<typeof Icons> = () => <List />;
+const List = () => {
+  const iconsNames = Object.keys(icons);
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-6 gap-4 h-fit-content">
+      {Object.values(icons).map((Icon: any, index) => (
+        <div
+          key={index}
+          className="flex flex-col justify-center items-center py-6 border-2 border-white/70 rounded-lg space-y-3"
+        >
+          <Icon className="fill-white/70 stroke-white/70" width={20} />
+          <div className="text-xxs truncate text-white/70">
+            {iconsNames[index]}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const IconLists: StoryFn<typeof Icons> = () => <List />;
