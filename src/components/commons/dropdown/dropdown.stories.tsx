26e6937addd889d9d8df53223ee3332c68d00b07
Hatta Alfaritzy <muh.hara16@gmail.com>
2025-01-07 03:26:13 +0700

    (master): feat: docs for dropdown n fix docs accordion

--- src/components/commons/dropdown/dropdown.stories.tsx
+++ src/components/commons/dropdown/dropdown.stories.tsx
@@ -1,25 +1,102 @@
-import React from "react";
 import type { Meta, StoryFn } from "@storybook/react";
 import { Dropdown } from ".";
 
-const meta: Meta<typeof Dropdown> = {
+export default {
   title: "Components/Commons/Dropdown",
   component: Dropdown as Meta<typeof Dropdown>["component"],
-  tags: ["autodocs"],
-  args: {
-    label: "Dropdown",
-    options: [
-      { label: "Option 1", value: "option1" },
-      { label: "Option 2", value: "option2" },
-      { label: "Option 3", value: "option3" },
-    ],
-  },
+} as Meta<typeof Dropdown>;
+
+export const DropdownDefault: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      label="Dropdown"
+      placeholder="Select an option"
+      options={[
+        { label: "Option 1", value: "1" },
+        { label: "Option 2", value: "2" },
+      ]}
+      onSelect={(option) => alert("Selected: " + option.label)}
+    />
+  );
 };
 
-export default meta;
-
-const Template: StoryFn<typeof Dropdown> = (args) => {
-  return <Dropdown {...args} />;
+export const DropdownLabel: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      label="Dropdown Label"
+      placeholder="Select an option"
+      options={[
+        { label: "Option 1", value: "1" },
+        { label: "Option 2", value: "2" },
+      ]}
+    />
+  );
 };
 
-export const Base = Template.bind({});
+export const DropdownImportant: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      label="Required Field"
+      important
+      placeholder="Select an option"
+      options={[
+        { label: "Option 1", value: "1" },
+        { label: "Option 2", value: "2" },
+      ]}
+    />
+  );
+};
+
+export const DropdownPlaceholder: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      placeholder="Choose an option"
+      options={[
+        { label: "Option A", value: "A" },
+        { label: "Option B", value: "B" },
+      ]}
+    />
+  );
+};
+
+export const DropdownOption: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      options={[
+        { label: "First Option", value: "1" },
+        { label: "Second Option", value: "2" },
+      ]}
+    />
+  );
+};
+
+export const DropdownInitialSelected: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      initialSelected={{ label: "Option 1", value: "1" }}
+      options={[
+        { label: "Option 1", value: "1" },
+        { label: "Option 2", value: "2" },
+      ]}
+    />
+  );
+};
+
+export const DropdownOnSelect: StoryFn<typeof Dropdown> = (args) => {
+  return (
+    <Dropdown
+      {...args}
+      options={[
+        { label: "Option 1", value: "1" },
+        { label: "Option 2", value: "2" },
+      ]}
+      onSelect={(option) => alert("Selected: " + option.label)}
+    />
+  );
+};
--- src/components/commons/dropdown/index.tsx
+++ src/components/commons/dropdown/index.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/utils/cn";
-import { useEffect, useState } from "react";
+import { useEffect, useState, forwardRef } from "react";
 import type { ButtonHTMLAttributes } from "react";
 import Icons from "../icons";
 
@@ -21,109 +21,115 @@ export interface IDropdownProps
   onSelect: (option: Option) => void;
 }
 
-export const Dropdown = ({
-  className,
-  classNameWrapper,
-  classNameLabel,
-  label,
-  important = false,
-  placeholder = "Select an option",
-  options,
-  initialSelected = null,
-  onSelect,
-  ...props
-}: IDropdownProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selectedOption, setSelectedOption] = useState<Option | null>(
-    initialSelected
-  );
+export const Dropdown = forwardRef<HTMLDivElement, IDropdownProps>(
+  (
+    {
+      className,
+      classNameWrapper,
+      classNameLabel,
+      label,
+      important = false,
+      placeholder = "Select an option",
+      options,
+      initialSelected = null,
+      onSelect,
+      ...props
+    }: IDropdownProps,
+    ref
+  ) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [selectedOption, setSelectedOption] = useState<Option | null>(
+      initialSelected
+    );
 
-  useEffect(() => {
-    const handleEscape = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        setIsOpen(false);
-      }
+    useEffect(() => {
+      const handleEscape = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          setIsOpen(false);
+        }
+      };
+      document.addEventListener("keydown", handleEscape);
+      return () => {
+        document.removeEventListener("keydown", handleEscape);
+      };
+    }, []);
+
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
     };
-    document.addEventListener("keydown", handleEscape);
-    return () => {
-      document.removeEventListener("keydown", handleEscape);
+
+    const handleSelectOption = (option: Option) => {
+      setSelectedOption(option);
+      setIsOpen(false);
+      onSelect(option);
     };
-  }, []);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleSelectOption = (option: Option) => {
-    setSelectedOption(option);
-    setIsOpen(false);
-    onSelect(option);
-  };
-
-  return (
-    <div className={cn("relative", classNameWrapper)}>
-      {label && (
-        <div
-          className={cn(
-            classNameLabel || "block font-semibold text-sm text-dark pb-1.5",
-            important && "after:content-['*'] text-error"
-          )}
-        >
-          {label}
-        </div>
-      )}
-      <button
-        type="button"
-        className={cn(
-          "flex items-center justify-between w-full px-4 py-2 bg-white border border-light rounded focus:border-dark",
-          className
+    return (
+      <div ref={ref} className={cn("relative", classNameWrapper)}>
+        {label && (
+          <div
+            className={cn(
+              classNameLabel ||
+                "block font-semibold text-sm text-dark dark:text-light pb-1.5",
+              important && "after:content-['*'] text-error"
+            )}
+          >
+            {label}
+          </div>
         )}
-        onClick={toggleDropdown}
-        {...props}
-      >
-        <span
+        <button
+          type="button"
           className={cn(
-            "text-sm font-medium",
-            selectedOption ? "text-dark" : "text-light"
+            "flex items-center justify-between w-full px-4 py-2 bg-white border border-light rounded focus:border-dark",
+            className
           )}
+          onClick={toggleDropdown}
+          {...props}
         >
-          {selectedOption ? selectedOption.label : placeholder}
-        </span>
-        <Icons
-          name="caret-down"
-          className={cn(
-            "fill-black cursor-pointer",
-            isOpen && "transform rotate-180"
-          )}
-        />
-      </button>
+          <span
+            className={cn(
+              "text-sm font-medium",
+              selectedOption ? "text-dark" : "text-dark/50"
+            )}
+          >
+            {selectedOption ? selectedOption.label : placeholder}
+          </span>
+          <Icons
+            name="caret-down"
+            className={cn(
+              "fill-black cursor-pointer",
+              isOpen && "transform rotate-180"
+            )}
+          />
+        </button>
 
-      {isOpen && (
-        <div className="absolute z-fixed left-0 mt-2 w-full p-1.5 rounded bg-white shadow-lg border border-dark/10">
-          {options.map((option) => (
-            <div
-              key={option.value}
-              className={cn(
-                "px-4 py-2 cursor-pointer hover:bg-light-300",
-                selectedOption === option && "bg-dark/95 hover:bg-dark",
-                "rounded"
-              )}
-              onClick={() => handleSelectOption(option)}
-            >
-              <span
+        {isOpen && (
+          <div className="absolute z-fixed left-0 mt-2 w-full p-1.5 rounded bg-white shadow-lg border border-dark/10">
+            {options.map((option) => (
+              <div
+                key={option.value}
                 className={cn(
-                  "text-sm",
-                  selectedOption === option ? "text-light" : "text-dark"
+                  "px-4 py-2 cursor-pointer hover:bg-light-300",
+                  selectedOption === option && "bg-dark/95 hover:bg-dark",
+                  "rounded"
                 )}
+                onClick={() => handleSelectOption(option)}
               >
-                {option.label}
-              </span>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
+                <span
+                  className={cn(
+                    "text-sm",
+                    selectedOption === option ? "text-light" : "text-dark"
+                  )}
+                >
+                  {option.label}
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Dropdown;
