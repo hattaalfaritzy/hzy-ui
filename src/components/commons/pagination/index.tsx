19891ee2b59dfc828d675310e07e389d05bf4740
Hatta Alfaritzy <muh.hara16@gmail.com>
2025-01-13 00:32:13 +0700

    (master): feat: docs for paginations

--- src/components/commons/pagination/index.tsx
+++ src/components/commons/pagination/index.tsx
@@ -1,8 +1,9 @@
+import { forwardRef } from "react";
 import { cn } from "@/utils/cn";
 import Button from "../button";
 import Icons from "../icons";
 
-interface PaginationProps {
+export interface PaginationProps {
   className?: string;
   currentPage: number;
   total: number;
@@ -10,130 +11,128 @@ interface PaginationProps {
   onPageChange: (pageNumber: number) => void;
 }
 
-export const Pagination = ({
-  className,
-  currentPage,
-  total,
-  itemsPerPage = 10,
-  onPageChange,
-}: PaginationProps) => {
-  const totalPages = Math.ceil(total / itemsPerPage);
+export const Pagination = forwardRef<HTMLDivElement, PaginationProps>(
+  ({ className, currentPage, total, itemsPerPage = 10, onPageChange }, ref) => {
+    const totalPages = Math.ceil(total / itemsPerPage);
 
-  const displayRange = currentPage === 1 || currentPage === totalPages ? 2 : 1;
+    const displayRange =
+      currentPage === 1 || currentPage === totalPages ? 2 : 1;
 
-  const getPageNumbers = () => {
-    const pages: (number | null)[] = [];
+    const getPageNumbers = () => {
+      const pages: (number | null)[] = [];
 
-    pages.push(1);
+      pages.push(1);
 
-    if (currentPage > displayRange + 2) {
-      pages.push(null);
-    }
-
-    for (
-      let i = currentPage - displayRange;
-      i <= currentPage + displayRange;
-      i++
-    ) {
-      if (i > 1 && i < totalPages) {
-        pages.push(i);
+      if (currentPage > displayRange + 2) {
+        pages.push(null);
       }
-    }
 
-    if (currentPage < totalPages - displayRange - 1) {
-      pages.push(null);
-    }
+      for (
+        let i = currentPage - displayRange;
+        i <= currentPage + displayRange;
+        i++
+      ) {
+        if (i > 1 && i < totalPages) {
+          pages.push(i);
+        }
+      }
 
-    if (totalPages > 1) {
-      pages.push(totalPages);
-    }
+      if (currentPage < totalPages - displayRange - 1) {
+        pages.push(null);
+      }
 
-    return pages;
-  };
+      if (totalPages > 1) {
+        pages.push(totalPages);
+      }
 
-  const handlePageChange = (pageNumber: number) => {
-    if (pageNumber !== currentPage) {
-      onPageChange(pageNumber);
-    }
-  };
+      return pages;
+    };
 
-  const startItem = (currentPage - 1) * itemsPerPage + 1;
-  const endItem = Math.min(currentPage * itemsPerPage, total);
+    const handlePageChange = (pageNumber: number) => {
+      if (pageNumber !== currentPage) {
+        onPageChange(pageNumber);
+      }
+    };
 
-  return (
-    <div
-      className={cn(
-        "flex flex-row justify-between items-center w-full p-8 bg-white",
-        className
-      )}
-    >
-      <span className="text-sm text-[#A5A5A5] font-regular">
-        Showing <b className="text-dark">{`${startItem}-${endItem}`}</b> from{" "}
-        <b className="text-dark">{total}</b> data
-      </span>
+    const startItem = (currentPage - 1) * itemsPerPage + 1;
+    const endItem = Math.min(currentPage * itemsPerPage, total);
+
+    return (
       <div
-        className={cn("flex flex-row justify-center items-center space-x-2")}
-      >
-        <Button
-          variant="dark"
-          outline={currentPage === 1 ? false : true}
-          className={cn(
-            "group rounded-xl h-10 w-10",
-            currentPage !== 1 && "hover:bg-gray-200"
-          )}
-          size="sm"
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          <Icons
-            name="chevron-left"
-            width={30}
-            className={cn(currentPage !== 1 ? "fill-dark" : "fill-white")}
-          />
-        </Button>
-        {getPageNumbers().map((pageNumber, index) =>
-          pageNumber === null ? (
-            <span key={index} className="px-3 py-1">
-              ...
-            </span>
-          ) : (
-            <Button
-              key={index}
-              variant="dark"
-              outline={pageNumber === currentPage ? false : true}
-              className={cn(
-                "rounded-xl h-10 w-10 text-sm p-0",
-                pageNumber === currentPage
-                  ? "hover:bg-dark/80"
-                  : "hover:bg-gray-200"
-              )}
-              size="md"
-              label={String(pageNumber)}
-              onClick={() => handlePageChange(pageNumber as number)}
-            />
-          )
+        ref={ref}
+        className={cn(
+          "flex flex-row justify-between items-center w-full p-8 bg-white",
+          className
         )}
-        <Button
-          variant="dark"
-          outline={currentPage === totalPages ? false : true}
-          className={cn(
-            "group rounded-xl h-10 w-10",
-            currentPage !== totalPages && "hover:bg-gray-200"
-          )}
-          size="sm"
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
+      >
+        <span className="text-sm text-[#A5A5A5] font-regular">
+          Showing <b className="text-dark">{`${startItem}-${endItem}`}</b> from{" "}
+          <b className="text-dark">{total}</b> data
+        </span>
+        <div
+          className={cn("flex flex-row justify-center items-center space-x-2")}
         >
-          <Icons
-            name="chevron-right"
+          <Button
+            variant="dark"
+            outline={currentPage === 1 ? false : true}
             className={cn(
-              currentPage !== totalPages ? "fill-dark" : "fill-white"
+              "group rounded-xl h-10 w-10",
+              currentPage !== 1 && "hover:bg-gray-200"
             )}
-          />
-        </Button>
+            size="sm"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <Icons
+              name="chevron-left"
+              width={30}
+              className={cn(currentPage !== 1 ? "fill-dark" : "fill-white")}
+            />
+          </Button>
+          {getPageNumbers().map((pageNumber, index) =>
+            pageNumber === null ? (
+              <span key={index} className="px-3 py-1">
+                ...
+              </span>
+            ) : (
+              <Button
+                key={index}
+                variant="dark"
+                outline={pageNumber === currentPage ? false : true}
+                className={cn(
+                  "rounded-xl h-10 w-10 text-sm p-0",
+                  pageNumber === currentPage
+                    ? "hover:bg-dark/80"
+                    : "hover:bg-gray-200"
+                )}
+                size="md"
+                label={String(pageNumber)}
+                onClick={() => handlePageChange(pageNumber as number)}
+              />
+            )
+          )}
+          <Button
+            variant="dark"
+            outline={currentPage === totalPages ? false : true}
+            className={cn(
+              "group rounded-xl h-10 w-10",
+              currentPage !== totalPages && "hover:bg-gray-200"
+            )}
+            size="sm"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            <Icons
+              name="chevron-right"
+              className={cn(
+                currentPage !== totalPages ? "fill-dark" : "fill-white"
+              )}
+            />
+          </Button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Pagination;
--- src/components/commons/pagination/pagination.stories.tsx
+++ src/components/commons/pagination/pagination.stories.tsx
@@ -1,34 +1,76 @@
-import React, { useState } from "react";
 import type { Meta, StoryFn } from "@storybook/react";
 import { Pagination } from ".";
 
-const meta: Meta<typeof Pagination> = {
+export default {
   title: "Components/Commons/Pagination",
   component: Pagination as Meta<typeof Pagination>["component"],
-  tags: ["autodocs"],
-  args: {
-    currentPage: 1,
-    total: 200,
-    itemsPerPage: 10,
-  },
-};
+} as Meta<typeof Pagination>;
 
-export default meta;
-
-const Template: StoryFn<typeof Pagination> = (args) => {
-  const [currentPage, setCurrentPage] = useState(args.currentPage);
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
+export const PaginationDefault: StoryFn<typeof Pagination> = (args) => {
   return (
     <Pagination
       {...args}
-      currentPage={currentPage}
-      onPageChange={handlePageChange}
+      currentPage={1}
+      total={50}
+      itemsPerPage={10}
+      onPageChange={(pageNumber) =>
+        console.log("Page changed to:" + pageNumber)
+      }
     />
   );
 };
 
-export const Base = Template.bind({});
+export const PaginationPageRanges: StoryFn<typeof Pagination> = (args) => {
+  return (
+    <Pagination
+      {...args}
+      currentPage={5}
+      total={100}
+      itemsPerPage={10}
+      onPageChange={(pageNumber) =>
+        console.log("Page changed to:" + pageNumber)
+      }
+    />
+  );
+};
+
+export const PaginationCustomStyling: StoryFn<typeof Pagination> = (args) => {
+  return (
+    <Pagination
+      {...args}
+      currentPage={1}
+      total={30}
+      className="bg-gray-100 p-4 rounded-md"
+      itemsPerPage={10}
+      onPageChange={(pageNumber) =>
+        console.log("Page changed to:" + pageNumber)
+      }
+    />
+  );
+};
+
+export const PaginationEventHandling: StoryFn<typeof Pagination> = (args) => {
+  return (
+    <Pagination
+      {...args}
+      currentPage={2}
+      total={60}
+      itemsPerPage={15}
+      onPageChange={(pageNumber) => alert("Navigated to page:" + pageNumber)}
+    />
+  );
+};
+
+export const PaginationVariants: StoryFn<typeof Pagination> = (args) => {
+  return (
+    <Pagination
+      {...args}
+      currentPage={1}
+      total={20}
+      itemsPerPage={5}
+      onPageChange={(pageNumber) =>
+        console.log("Page changed to:" + pageNumber)
+      }
+    />
+  );
+};
