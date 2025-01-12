23fe54c7eefdc34ccee44c57d417b8294f3fc1f7
Hatta Alfaritzy <muh.hara16@gmail.com>
2025-01-13 00:55:05 +0700

    (master): feat: docs for tags

--- src/components/commons/tag/index.tsx
+++ src/components/commons/tag/index.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/utils/cn";
-import type { HTMLAttributes } from "react";
+import { forwardRef, HTMLAttributes } from "react";
 
 export interface ITagProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
@@ -9,46 +9,45 @@ export interface ITagProps extends HTMLAttributes<HTMLDivElement> {
   onClick?: () => void;
 }
 
-export const Tag = ({
-  className,
-  classNameLabel,
-  label,
-  variant = "error",
-  onClick,
-  ...props
-}: ITagProps) => {
-  if (!label) return null;
+export const Tag = forwardRef<HTMLDivElement, ITagProps>(
+  (
+    { className, classNameLabel, label, variant = "error", onClick, ...props },
+    ref
+  ) => {
+    if (!label) return null;
 
-  const style = {
-    dark: "bg-dark",
-    light: "bg-light text-dark",
-    success: "bg-success",
-    informative: "bg-informative",
-    warning: "bg-warning",
-    error: "bg-error",
-  }[variant];
+    const style = {
+      dark: "bg-dark",
+      light: "bg-light text-dark",
+      success: "bg-success",
+      informative: "bg-informative",
+      warning: "bg-warning",
+      error: "bg-error",
+    }[variant];
 
-  return (
-    <div
-      className={cn(
-        "flex justify-center items-center rounded-lg px-3 py-1.5 bg-opacity-90 w-full space-x-2 max-w-fit",
-        style,
-        className
-      )}
-      onClick={onClick}
-      {...props}
-    >
+    return (
       <div
+        ref={ref}
         className={cn(
-          "flex-1 text-xs",
-          variant === "light" ? "text-dark" : "text-white",
-          classNameLabel
+          "flex justify-center items-center rounded-lg px-3 py-1.5 bg-opacity-90 w-full space-x-2 max-w-fit",
+          style,
+          className
         )}
+        onClick={onClick}
+        {...props}
       >
-        {label}
+        <div
+          className={cn(
+            "flex-1 text-xs",
+            variant === "light" ? "text-dark" : "text-white",
+            classNameLabel
+          )}
+        >
+          {label}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Tag;
--- src/components/commons/tag/tag.stories.tsx
+++ src/components/commons/tag/tag.stories.tsx
@@ -1,23 +1,46 @@
-import React from "react";
 import type { Meta, StoryFn } from "@storybook/react";
 import { Tag } from ".";
 
-const meta: Meta<typeof Tag> = {
+export default {
   title: "Components/Commons/Tag",
   component: Tag as Meta<typeof Tag>["component"],
-  tags: ["autodocs"],
-  argTypes: {
-    variant: {
-      options: ["dark", "light", "informative", "success", "warning", "error"],
-      control: { type: "radio" },
-    },
-  },
+} as Meta<typeof Tag>;
+
+export const TagDefault: StoryFn<typeof Tag> = (args) => {
+  return <Tag {...args} label="Default Tag" />;
 };
 
-export default meta;
-
-const Template: StoryFn<typeof Tag> = (args) => {
-  return <Tag label="Tag Text" {...args} />;
+export const TagVariants: StoryFn<typeof Tag> = (args) => {
+  return (
+    <div className="custom-canvas">
+      <Tag label="Dark Tag" variant="dark" {...args} />
+      <Tag label="Light Tag" variant="light" {...args} />
+      <Tag label="Informative Tag" variant="informative" {...args} />
+      <Tag label="Success Tag" variant="success" {...args} />
+      <Tag label="Warning Tag" variant="warning" {...args} />
+      <Tag label="Error Tag" variant="error" {...args} />
+    </div>
+  );
 };
 
-export const Base = Template.bind({});
+export const TagCustomClass: StoryFn<typeof Tag> = (args) => {
+  return (
+    <Tag
+      {...args}
+      label="Custom Styled Tag"
+      className="bg-purple-500"
+      classNameLabel="text-white font-bold"
+    />
+  );
+};
+
+export const TagOnClick: StoryFn<typeof Tag> = (args) => {
+  return (
+    <Tag
+      {...args}
+      label="Clickable Tag"
+      variant="informative"
+      onClick={() => alert("Tag clicked!")}
+    />
+  );
+};
